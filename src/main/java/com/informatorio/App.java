9289325f52b9184fd55f66fe07a0e9e092167300
Ariel <alarmoreira@gmail.com>
2023-11-22 19:53:49 -0300

    Se crea y configura proyecto.

---
+++ src/main/java/com/informatorio/App.java
@@ -0,0 +1,12 @@
+package com.informatorio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Configurando el proyecto Maven" );
+    }
+}
